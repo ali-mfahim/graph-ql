99f90e3bc38459295e7dbaf42814f8fee8dff35a
ali-mfahim <sm.ali10@yahoo.com>
2024-02-19 06:11:22 +0500

    create new product with new media images mutation

--- app/Http/Controllers/GraphQlController.php
+++ app/Http/Controllers/GraphQlController.php
@@ -22,7 +22,8 @@ class GraphQlController extends Controller
     }
     public function fetchProducts()
     {
-        $project = $this->getStoreDetails();
+      $project = $this->getStoreDetails();
+      $url = $project->base_url . $project->api_version ."/graphql.json";
         $query = '
         {
             products(first: 1) {
@@ -59,7 +60,7 @@ class GraphQlController extends Controller
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
             'X-Shopify-Access-Token' => $project->access_token,
-        ])->post('https://40770a-ce.myshopify.com/admin/api/2024-01/graphql.json', [
+        ])->post( $url , [
             'query' => $query,
         ]);
         $response = $response->json();
@@ -70,19 +71,32 @@ class GraphQlController extends Controller
 
     public function createProduct(Request $request)
     {
+        // Get necessary data for the request
         $project = $this->getStoreDetails();
-      
+        $url = $project->base_url . $project->api_version . "/graphql.json";
+
+        // Construct the GraphQL mutation for creating a product
         $mutation = '
             mutation {
                 productCreate(input: {
                     title: "' . $request->title . '",
                     descriptionHtml: "' . $request->description . '",
+                    productType:"TEST PRODUCT",
+                  
+                    variants: [
+                        {
+                            title: "' . $request->variant_title . '",
+                            price: "' . $request->price . '"
+                        }
+                    ],
                 }) {
                     product {
                         id
                         title
-                        description
-                        
+                        productType
+                        descriptionHtml
+
+
                     }
                     userErrors {
                         field
@@ -91,13 +105,88 @@ class GraphQlController extends Controller
                 }
             }
         ';
+
+        // Make the request to Shopify GraphQL API
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
             'X-Shopify-Access-Token' => $project->access_token,
-        ])->post('https://40770a-ce.myshopify.com/admin/api/2024-01/graphql.json', [
+        ])->post($url, [
             'query' => $mutation,
         ]);
 
-        return $response->json();
+        // Return the response from Shopify API
+        $data = $response->json();
+        if(isset($data['data']['productCreate']['product']) && !empty($data['data']['productCreate']['product'])) {
+          $product = $data['data']['productCreate']['product'];
+          $product_id = $product['id'];
+          $mediaUrls = [
+            "https://patpatwebstatic.s3.us-west-2.amazonaws.com/origin/product/000000000000/63dc80e2b9b4b.jpg",
+            "https://patpatwebstatic.s3.us-west-2.amazonaws.com/origin/product/000000000000/63dc80e29e094.jpg",
+            // Add more image URLs as needed
+        ];  
+          $saveImage = $this->createProductMedia($product_id, $mediaUrls);
+          return [
+            $saveImage
+          ];
+        }else{
+          return $data['data']['productCreate']['userErrors'];
+        }
+        return ;
     }
+
+
+      public function createProductMedia($productId, $mediaUrls)
+      {
+          // Get necessary data for the request
+          $project = $this->getStoreDetails();
+          $url = $project->base_url . $project->api_version . "/graphql.json";
+
+          // Construct the GraphQL mutation for creating product media
+          $mutation = '
+            mutation ($media: [CreateMediaInput!]!, $productId: ID!) {
+                productCreateMedia(media: $media, productId: $productId) {
+                    media {
+                        id
+                    }
+                    mediaUserErrors {
+                        code
+                        field
+                        message
+                    }
+                    product {
+                        id
+                        title
+                        # Add more fields you want to retrieve for the product
+                    }
+                }
+            }
+        ';
+          // Define variables for the mutation
+          $variables = [
+            'media' => [],
+            'productId' => base64_encode($productId), // Encode product ID
+          ];
+
+          // Prepare media input
+          foreach ($mediaUrls as $mediaUrl) {
+            $variables['media'][] = [
+                'mediaContentType' => 'IMAGE',
+                'originalSource' => $mediaUrl, // Change 'src' to 'originalSource'
+            ];
+          }
+
+          // Make the request to Shopify GraphQL API
+          $response = Http::withHeaders([
+              'Content-Type' => 'application/json',
+              'X-Shopify-Access-Token' => $project->access_token,
+          ])->post($url, [
+              'query' => $mutation,
+              'variables' => $variables,
+          ]);
+
+          // Return the response from Shopify API
+          return $response->json();
+      }
+
+
 }
